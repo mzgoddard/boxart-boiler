8839f0cdd116cdf74ca5931fcc612fc8900b0002
K. Adam White <kadamwhite@users.noreply.github.com>
2016-05-13 12:35:41 -0400

    Basic transition demonstration: one container to another

--- src/modules/main/index.jsx
+++ src/modules/main/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {Animated, AnimatedAgent} from 'boxart';
+
 import Component from '../update-ancestor';
 
 class Main extends Component {
@@ -8,14 +10,46 @@ class Main extends Component {
     super();
 
     this.state = {
-
+      pos: 'left'
     };
   }
 
+  rerender() {
+    this.setState({
+      pos: this.state.pos === 'left' ? 'right' : 'left'
+    });
+  }
+
   render() {
+    var pos1, pos2;
+    // To use a custom duration:
+    // <Animated animateKey="h1" animate={(options) => {
+    //   return options.animateFromLast(2);
+    // }}>
+    var card = (
+      <Animated animateKey="h1">
+        <div className="card">A &hearts;</div>
+      </Animated>
+    );
+
+    if (this.state.pos === 'right') {
+      pos1 = card;
+    } else {
+      pos2 = card;
+    }
+
     return (
-      <div className="game-board">
-      </div>
+      <AnimatedAgent>
+        <div className="game-board">
+          <div className="container" id="container1">
+            {pos1}
+          </div>
+          <div className="container" id="container2">
+            {pos2}
+          </div>
+          <button type="button" onClick={this.rerender}>Shift It</button>
+        </div>
+      </AnimatedAgent>
     );
   }
 }
